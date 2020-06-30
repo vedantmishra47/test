a67e2ad733ca860fb5610c9a262d239467721eb3
vedant mishra <vedantmishra6190@gmail.com>
2020-06-30 15:30:57 +0530

    Transactional management

--- src/main/java/com/example/test/StudentService.java
+++ src/main/java/com/example/test/StudentService.java
@@ -4,11 +4,13 @@ import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
 @Service
+@Transactional
 public class StudentService {
     @Autowired
     private StudentRepository studentRepository;
